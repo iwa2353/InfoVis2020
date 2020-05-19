4a067c9f42a2b668f5c6a51c0b6c3f8e59c7bf9f
IWAHARANaoya <iwa2353@MBPro-15-180115.local>
2020-05-19 13:47:24 +0900

    ?th commit

--- W02/vec3.js
+++ W02/vec3.js
@@ -1,4 +1,5 @@
 //Class
+class Vec3
 {
     constructor(x,y,z)
     {
@@ -13,11 +14,38 @@
 	this.z += v.z;
 	return this;
     }
+    
     sum(){
 	this.x + this.y + this.z;
 	return this;
-    }    
-}
+    }
+    
+    min(){
+	var min = Math.min(this.x,this.y,this.z)// Math method
+	//var min = this.x<this.y?(this.x<this.z?this.x:this.z):(this.y<this.z?this.y:this.z);//三項演算子
 
+	/*if(this.x<this./y){  //if and else
+	    if(this.x<this.z){
+		min = this.x;
+	    }else{
+		min = this.z;
+	    }
+	}else if(this.y<this.z){
+	    min = this.y;
+	}else{
+	    min = this.z;
+	}*/
+	
+	return min;
+    }
+  
+    max(){
+	return  this.x>this.y?(this.x>this.z?this.x:this.z):(this.y>this.z?this.y:this.z)
+    }
+    
+    mid(){
+	return (this.x!=this.min())&&(this.x!=this.max())?this.x:(this.y!=this.min())&&(this.y!=this.max())?this.y:this.z//見にくい
+    }
+}
 
 
